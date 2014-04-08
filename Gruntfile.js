8af2201defa7a0bcf346ccac6cc5cb9817cf6603
hypexr <scott@hypexr.org>
2014-04-07 18:55:11 -0700

    Updated test target to run jshint since travisci runs the test task by default

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,6 +68,7 @@ module.exports = function(grunt) {
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
   grunt.registerTask('test', [
+    'jshint',
     'clean',
     'copy',
     'versionCopyBowerComponents',
@@ -75,7 +76,7 @@ module.exports = function(grunt) {
   ]);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['test']);
 
 };
 
